91efd6baabfc882885d6bb36b59c9c42ad32fc01
Alex Casson <alex.casson@manchester.ac.uk>
2026-01-17 10:28:06 +0000

    Revert "Added analytics"
    This reverts commit d9770dd30710b3dc60f9dd38e48c2edc5cc0deea.

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,3 @@
-import os
-
-
 # General project setup
 project = "notes-part0"
 copyright = '2026 The University of Manchester. <a class="nav-link text-light" href="https://creativecommons.org/licenses/by-nc-sa/4.0/">Released under CC-BY-NC-SA 4.0 license.</a>'
@@ -23,11 +20,6 @@ extensions = [
     "sphinx_togglebutton",
     "sphinx_prompt",
 ]
-
-
-if os.getenv("GITHUB_ACTIONS"):
-  extensions.append("sphinxcontrib.googleanalytics")
-  googleanalytics_id = "G-HKCDFDZSK7"
 
 
 # Theme settings
